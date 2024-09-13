1db3e7e4be8529b9753e804b8dc97c5c9ec242de
Lamrofen <121419102+Lamrofen89@users.noreply.github.com>
2024-09-13 15:55:34 +0300

    Update zadanie12.sh
    Final version of the code

--- zadanie12.sh
+++ zadanie12.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-items=$(ls -A /var/)
+get_zad12() {
+    local path="$1"
+    local size_dm=$(du -hs "$path" 2>/dev/null | cut -f1)
+    echo $size_dm
+}
+
+items=$(ls -A)
 
 for item in $items; do
-    echo $item
+    size_dm=$(get_zad12 "$item")
+    result+=("$size_dm $item")
 done
+
+printf "%s\n" "${result[@]}" | sort -rh | column -t
